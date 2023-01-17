67e3c74ff794b002e0a375297239603badb55a6a
JesseHoch <jesse.hoch@linuxacademy.com>
2023-01-17 16:37:59 -0500

    Added the GKE Terraform Config

---
+++ gke/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "playground-s-11-188ce37f"
+region     = "us-east1"
---
+++ gke/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.27.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+}
